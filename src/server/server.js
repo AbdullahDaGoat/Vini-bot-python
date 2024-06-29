1ed547d3d3d9add06213d8784d218e4f36d3bc04
Abdullah <abdullahaviator13@gmail.com>
2024-06-29 03:35:34 -0400

    Code broke gotta fix tmrw

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const authRoutes = require('./routes/auth');
-const { isAuthenticated } = require('./middleware/auth');
+const  isAuthenticated  = require('./middleware/auth');
 
 const app = express();
 
