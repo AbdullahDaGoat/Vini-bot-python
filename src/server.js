ba26567a87f62bcaf6d93642c6bc0dfbb4cfaf20
Abdullah <abdullahaviator13@gmail.com>
2024-07-28 18:39:45 -0400

    s

--- src/server.js
+++ src/server.js
@@ -51,14 +51,14 @@ async function logError(title, error) {
 
 // JWT middleware
 function authenticateToken(req, res, next) {
-  const token = req.cookies.token;
+  // const token = req.cookies.token;
 
-  if (token == null) {
-    console.log('No token provided');
-    return res.sendStatus(401);
-  }
+  // if (token == null) {
+  //   console.log('No token provided');
+  //   return res.sendStatus(401);
+  // }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(process.env.JWT_SECRET, (err, user) => {
     if (err) {
       console.log('Token verification failed:', err);
       return res.sendStatus(403);
